fe01bf0c4803a01c542b8acf6a6bee9ee64d1647
jrofor <47061951+jrofor@users.noreply.github.com>
2019-03-27 23:09:10 +0300

    NewsViewHolder

--- app/src/main/java/com/example/roman/listofnews/ui/adapter/NewsViewHolder.java
+++ app/src/main/java/com/example/roman/listofnews/ui/adapter/NewsViewHolder.java
@@ -3,10 +3,27 @@ package com.example.roman.listofnews.ui.adapter;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.bumptech.glide.RequestManager;
+import com.example.roman.listofnews.R;
 
 public class NewsViewHolder extends RecyclerView.ViewHolder {
 
-    public NewsViewHolder(@NonNull View itemView) {
+    private RequestManager imageLoader;
+    private ImageView multimedia;
+    private ProgressBar progressBar;
+    private static final int LayuotItem = R.layout.item_news;
+
+    public NewsViewHolder(@NonNull View itemView, RequestManager glideRequestManager) {
         super(itemView);
+        this.imageLoader = glideRequestManager;
+        findViews(itemView);
     }
+
+    private void findViews (@NonNull View itemView) {
+
+    }
+
 }
