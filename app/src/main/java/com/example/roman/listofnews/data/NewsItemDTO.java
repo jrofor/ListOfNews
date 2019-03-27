546fa075a39069bc45d81c99b7f52ac969aae65c
jrofor <rofor.jj@gmail.com>
2019-03-27 16:50:32 +0300

    add NewsItemDTO

---
+++ app/src/main/java/com/example/roman/listofnews/data/NewsItemDTO.java
@@ -0,0 +1,65 @@
+package com.example.roman.listofnews.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import java.util.Date;
+
+public class Example {
+
+    @SerializedName("subsection")
+    @Expose
+    private String subsection;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("abstract")
+    @Expose
+    private String _abstract;
+    @SerializedName("updated_date")
+    @Expose
+    private Date updatedDate;
+    @SerializedName("multimedia")
+    @Expose
+    private String multimedia;
+
+    public String getSubsection() {
+        return subsection;
+    }
+
+    public void setSubsection(String subsection) {
+        this.subsection = subsection;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAbstract() {
+        return _abstract;
+    }
+
+    public void setAbstract(String _abstract) {
+        this._abstract = _abstract;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getMultimedia() {
+        return multimedia;
+    }
+
+    public void setMultimedia(String multimedia) {
+        this.multimedia = multimedia;
+    }
+
+}
