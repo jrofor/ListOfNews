98bddcbf79d543ab6aef2d2ee092a3f76534ff26
jrofor <47061951+jrofor@users.noreply.github.com>
2019-03-27 21:38:28 +0300

    Update NewsItemDTO.java

--- app/src/main/java/com/example/roman/listofnews/data/NewsItemDTO.java
+++ app/src/main/java/com/example/roman/listofnews/data/NewsItemDTO.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import java.util.Date;
 
-public class Example {
+public class NewsItemDTO {
 
     @SerializedName("subsection")
     @Expose
