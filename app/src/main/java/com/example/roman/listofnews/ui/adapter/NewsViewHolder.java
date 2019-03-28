376e27d9e11a22602dc8a7f3077b4ade21c926ff
jrofor <47061951+jrofor@users.noreply.github.com>
2019-03-28 20:25:32 +0300

    create findViews

--- app/src/main/java/com/example/roman/listofnews/ui/adapter/NewsViewHolder.java
+++ app/src/main/java/com/example/roman/listofnews/ui/adapter/NewsViewHolder.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.bumptech.glide.RequestManager;
 import com.example.roman.listofnews.R;
@@ -12,8 +13,13 @@ import com.example.roman.listofnews.R;
 public class NewsViewHolder extends RecyclerView.ViewHolder {
 
     private RequestManager imageLoader;
-    private ImageView multimedia;
     private ProgressBar progressBar;
+    private TextView tvSubsection;
+    private TextView tvTitle;
+    private TextView tvPreview;
+    private ImageView ivMultimedia;
+    private TextView tvDate;
+
     private static final int LayuotItem = R.layout.item_news;
 
     public NewsViewHolder(@NonNull View itemView, RequestManager glideRequestManager) {
@@ -23,6 +29,11 @@ public class NewsViewHolder extends RecyclerView.ViewHolder {
     }
 
     private void findViews (@NonNull View itemView) {
+        ivMultimedia = itemView.findViewById(R.id.iv_multimedia);
+        progressBar = itemView.findViewById(R.id.progress_bar);
+        tvSubsection = itemView.findViewById(R.id.tv_subsection);
+        tvTitle = itemView.findViewById(R.id.tv_title);
+        tvDate = itemView.findViewById(R.id.tv_date);
 
     }
 
