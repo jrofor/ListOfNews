c92d3b21e7e4eacad11589478b8fe40ef325dbeb
jrofor <47061951+jrofor@users.noreply.github.com>
2019-03-30 17:09:19 +0300

    change NewsMain add State add DefResponse

--- app/src/main/java/com/example/roman/listofnews/NewsMainActivity.java
+++ app/src/main/java/com/example/roman/listofnews/NewsMainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 
 import android.support.annotation.NonNull;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import android.content.res.Configuration;
@@ -11,24 +12,41 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.roman.listofnews.data.DataUtils;
+import com.bumptech.glide.Glide;
+import com.example.roman.listofnews.data.NewsItemDTO;
 import com.example.roman.listofnews.ui.adapter.NewsRecyclerAdapter;
+import com.example.roman.listofnews.ui.State;
+import com.example.roman.listofnews.ux.DefaultResponse;
 
+import java.util.List;
 import java.util.Objects;
 
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import retrofit2.Call;
 
 public class NewsMainActivity extends AppCompatActivity {
 
-    private final NewsRecyclerAdapter.OnItemClickListener  clickListener = position -> {
+    private RecyclerView rvNews;
+    private NewsRecyclerAdapter NewsAdapter;
+    private View viewLoading;
+    private View viewNoDate;
+    private View viewError;
+    private Button btnTryAgain;
 
-    final String EXTRA_MESSAGE = "extra:news";
-    Intent intent = new Intent(this, NewsDetailsActivity.class);
-    intent.putExtra(EXTRA_MESSAGE, position);
-    startActivity(intent);
+    @Nullable
+    private Call<DefaultResponse<List<NewsItemDTO>>> ResponseTopStoriesHome;
+
+    private final NewsRecyclerAdapter.OnItemClickListener  clickListener = position -> {
+        final String EXTRA_MESSAGE = "extra:news";
+        Intent intent = new Intent(this, NewsDetailsActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, position);
+        startActivity(intent);
     };
 
 
@@ -39,17 +57,66 @@ public class NewsMainActivity extends AppCompatActivity {
         setupUi();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setupUx();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unbindUx();
+    }
+
+    private void unbindUx() {
+        btnTryAgain.setOnClickListener(null);
+    }
+
     private void setupUi() {
         findView();
+        setupRecyclerViews();
+    }
+
+    private void setupUx() {
+        loadTopStoriesHome();
+
+    }
+
+
+    private void loadTopStoriesHome() {
+        showState(State.Loading);
+
+
+    }
+
+    public void showState(@NonNull State state) {
+        switch (state) {
+            case Loading:
+                rvNews.setVisibility(View.GONE);
+                viewError.setVisibility(View.GONE);
+                viewNoDate.setVisibility(View.GONE);
+
+                viewLoading.setVisibility(View.VISIBLE);
+                break;
+        }
 
     }
 
     private void findView() {
-        RecyclerView recyclerView = findViewById(R.id.rv_news);
-        recyclerView.setAdapter(new NewsRecyclerAdapter(this, DataUtils.generateNews(), clickListener));
-        int orientation = getResources().getConfiguration().orientation;
-        onChangeColumnsWithOrientation(orientation, recyclerView);
+        rvNews = findViewById(R.id.rv_news);
+        viewLoading = findViewById(R.id.fl_loading);
+        viewNoDate = findViewById(R.id.fl_no_data);
+        viewError = findViewById(R.id.ll_error);
+        btnTryAgain = findViewById(R.id.bnt_try_again);
 
+    }
+
+    private void setupRecyclerViews() {
+        NewsAdapter = new NewsRecyclerAdapter(Glide.with(this), clickListener);
+        rvNews.setAdapter(NewsAdapter);
+        int orientation = getResources().getConfiguration().orientation;
+        onChangeColumnsWithOrientation(orientation, rvNews);
     }
 
     public void onChangeColumnsWithOrientation(int orientation, RecyclerView recyclerView) {
---
+++ app/src/main/java/com/example/roman/listofnews/ux/DefaultResponse.java
@@ -0,0 +1,12 @@
+package com.example.roman.listofnews.ux;
+
+import android.support.annotation.Nullable;
+
+public class DefaultResponse<T> {
+    private T data;
+
+    @Nullable
+    public T getData(){
+        return data;
+    }
+}
